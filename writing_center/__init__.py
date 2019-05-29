97391cf9c7988824225c2a4875a51302f3ab5be5
Jordan Jasik <joj28267@bethel.edu>
2019-05-29 15:40:43 -0500

    Skeleton homepage
    I copied over a lot of designs from tutorlabs, but that is not necessarily the way things will stay. The basic idea of the nav is how I imagine it will be though.

--- writing_center/__init__.py
+++ writing_center/__init__.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask import session as flask_session
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.debug = True
 
 from writing_center.views import View
 View.register(app)
+
+
+# This makes these variables open to use everywhere
+@app.context_processor
+def utility_processor():
+    to_return = {}
+    to_return.update({
+        'now': datetime.now(),
+    })
+
+    return to_return
+
+
+@app.before_request
+def before_request():
+    flask_session['NAME'] = app.config["TEST_NAME"]
+
 
 if __name__ == "__main__":
     app.run()
