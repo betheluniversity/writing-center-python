1d1ea3ee490a32d86f7ac8e51307053e493e4f64
Jordan Jasik <joj28267@bethel.edu>
2019-06-03 14:48:16 -0500

    Settings template

--- writing_center/email_tab/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask_classy import FlaskView
-
-
-class EmailView(FlaskView):
-    def __init__(self):
-        pass
--- writing_center/settings/__init__.py
+++ writing_center/settings/__init__.py
@@ -1,6 +1,11 @@
-from flask_classy import FlaskView
+from flask import render_template
+from flask_classy import FlaskView, route
 
 
 class SettingsView(FlaskView):
     def __init__(self):
         pass
+
+    @route('/')
+    def index(self):
+        return render_template('settings/index.html')
