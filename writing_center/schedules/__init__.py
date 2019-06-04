1ac4babf990415320e980f92cbbc971cccfd8e46
Jordan Jasik <joj28267@bethel.edu>
2019-06-04 10:20:45 -0500

    Create Schedule Template

--- writing_center/schedules/__init__.py
+++ writing_center/schedules/__init__.py
@@ -1,6 +1,11 @@
-from flask_classy import FlaskView
+from flask_classy import FlaskView, route
+from flask import render_template
 
 
 class SchedulesView(FlaskView):
     def __init__(self):
         pass
+
+    @route("/create")
+    def create_schedule(self):
+        return render_template("schedules/create_schedule.html")
