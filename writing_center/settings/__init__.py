812acff13971db193711ad10eab06d3cfcbd6ef7
Jordan Jasik <joj28267@bethel.edu>
2019-06-04 14:35:26 -0500

    Database is hooked up

--- writing_center/settings/__init__.py
+++ writing_center/settings/__init__.py
@@ -1,11 +1,14 @@
 from flask_classy import FlaskView, route
 from flask import render_template
 
+from writing_center.settings.settings_controller import SettingsController
+
 
 class SettingsView(FlaskView):
     def __init__(self):
-        pass
+        self.sc = SettingsController()
 
     @route('/')
     def index(self):
-        return render_template('settings/index.html')
+        settings = self.sc.get_settings()
+        return render_template('settings/index.html', **locals())
