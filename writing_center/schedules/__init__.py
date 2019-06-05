ba41152b2476aaf0a48d6a94aed0cef50d59f05a
Bailey Middendorf <bam95899@bethel.edu>
2019-06-05 08:29:34 -0500

    manage tutor schedules template

--- writing_center/schedules/__init__.py
+++ writing_center/schedules/__init__.py
@@ -9,3 +9,7 @@ class SchedulesView(FlaskView):
     @route("/create")
     def create_schedule(self):
         return render_template("schedules/create_schedule.html")
+
+    @route('/center-manager/manage-tutor-schedules')
+    def manage_tutor_schedules(self):
+        return render_template('schedules/manage_tutor_schedules.html', **locals())
