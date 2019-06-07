fb5d36788c31d39f928ec0c4f65b9ec3a8eb096a
Boston KJ <bak45247@bethel.edu>
2019-06-07 10:42:56 -0500

    refactoring and added user_functions file

--- writing_center/db_repository/message_center_functions.py
+++ writing_center/db_repository/message_center_functions.py
@@ -2,10 +2,10 @@ from datetime import datetime, timedelta
 from sqlalchemy import orm
 from flask import session as flask_session
 from writing_center.db_repository import db_session
-from writing_center.db_repository.tables import WCEmailPreferencesTable
+from writing_center.db_repository.tables import WCEmailPreferencesTable, WCAppointmentDataTable, WCDropInAppointmentsTable
 
 
-class Email_Preferences:
+class Message_Center:
     def get_email_preferences(self, user_id):
         return (db_session.query(WCEmailPreferencesTable)
                 .filter(WCEmailPreferencesTable.id == user_id)
@@ -16,3 +16,8 @@ class Email_Preferences:
         row.SubRequestEmail = substitute
         row.StudentSignupEmail = shift
         db_session.commit()
+
+    def get_appointment_info(self, appointment_id):
+        pass
+
+
--- writing_center/message_center/message_center_controller.py
+++ writing_center/message_center/message_center_controller.py
@@ -5,31 +5,30 @@ from flask_mail import Mail, Message
 
 # Local
 from writing_center import app
-from writing_center.db_repository.email_preference_functions import Email_Preferences
+from writing_center.db_repository.message_center_functions import Message_Center
 
 
 class MessageCenterController:
 
     def __init__(self):
-        self.email_preferences = Email_Preferences()
+        self.message_center = Message_Center()
 
     def manage_message_preferences(self, substitute, shift):
-        self.email_preferences.change_email_preferences(substitute, shift, session['user_id'])
+        self.message_center.change_email_preferences(substitute, shift, session['user_id'])
         return render_template('message_center/preferences.html', **locals())
 
     def get_message_preferences(self):
-        return self.email_preferences.get_email_preferences(session['user_id'])
+        return self.message_center.get_email_preferences(session['user_id'])
 
-    def close_session_email(self, session_id):  # TODO: refactor to work with appointments
-        sess = self.session.get_session(session_id)
-        subject = '{{{0}}} {1} ({2})'.format(app.config['LAB_TITLE'], sess.name, sess.date.strftime('%m/%d/%Y'))
-        opener = self.user.get_user(sess.openerId)
-        tutors = self.session.get_session_tutors(session_id)
-        recipients = self.user.get_end_of_session_recipients()
-        session_courses = self.course.get_courses_for_session(session_id)
+    def close_session_email(self, appointment_id):  # TODO: refactor to work with appointments
+        appointment = self.message_center.get_appointment_info(appointment_id)
+        subject = '{{{0}}} {1} ({2})'.format(app.config['LAB_TITLE'], appointment.StudentUsername, appointment.date.strftime('%m/%d/%Y'))
+        tutor = appointment.TutorUsername
+        recipients = self.user.get_end_of_session_recipients()  # Not sure what to put here
+        session_courses = self.course.get_courses_for_session(session_id)  # Not sure what toput here
 
         for recipient in recipients:
-            recipient_roles = self.user.get_user_roles(recipient.id)
+            recipient_roles = self.user.get_user_roles(recipient.id)  # will use the same people as recipient
             recipient_role_names = []
 
             for role in recipient_roles:
@@ -40,13 +39,11 @@ class MessageCenterController:
             if 'Administrator' in recipient_role_names:
                 session_students = self.session.get_session_students(session_id)
                 for student in session_students:
-                    students_and_courses[student] = self.session.get_report_student_session_courses(session_id,
-                                                                                                    student.id)  # Gets courses attended
+                    students_and_courses[student] = self.session.get_report_student_session_courses(session_id, student.id)  # Gets courses attended
 
                 for course in session_courses:
                     courses_and_info[course] = {}
-                    courses_and_info[course]['students'] = self.session.get_session_course_students(session_id,
-                                                                                                    course.id)  # Gets students for a course in a specific session
+                    courses_and_info[course]['students'] = self.session.get_session_course_students(session_id, course.id)  # Gets students for a course in a specific session
                     courses_and_info[course]['profs'] = self.course.get_profs_from_course(course.id)
 
             else:  # They must be a prof since get_end_of_session_recipients only gets admins and profs
@@ -68,14 +65,7 @@ class MessageCenterController:
                     students_and_courses[student] = self.session.get_report_student_session_courses(session_id,
                                                                                                     student.id)  # Same as above
 
-            attendance = 0
-            for course, info in courses_and_info.items():
-                for student in info['students']:
-                    attendance = attendance + 1
-            if attendance > 0 or 'Administrator' in recipient_role_names:
-                # send an email
-                self.send_message(subject, render_template('sessions/email.html', **locals()), recipient.email, None,
-                                  True)
+            self.send_message(subject, render_template('sessions/email.html', **locals()), recipient.email, None, True)
 
     def send_message(self, subject, body, recipients, bcc, html=False):
         if app.config['ENVIRON'] != 'prod':
