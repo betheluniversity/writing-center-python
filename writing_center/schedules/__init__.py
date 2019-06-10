68a90dbe526003302ccb6e241ff9062de112532d
Bailey Middendorf <bam95899@bethel.edu>
2019-06-10 13:56:44 -0500

    Added "Add Schedule Time Slot" Functionality

--- writing_center/schedules/__init__.py
+++ writing_center/schedules/__init__.py
@@ -1,14 +1,23 @@
-from flask_classy import FlaskView, route
-from flask import render_template
+from flask_classy import FlaskView, route, request
+from flask import render_template, redirect, url_for
+from datetime import datetime
+
+import json
+
+from writing_center.schedules.schedules_controller import SchedulesController
 
 
 class SchedulesView(FlaskView):
-    def __init__(self):
-        pass
+    route_base = '/schedules/'
 
-    @route("/create")
+    def __init__(self):
+        self.sc = SchedulesController()
+
+    @route("/create-schedule")
     def create_schedule(self):
-        return render_template("schedules/create_schedule.html")
+        schedules = self.sc.get_schedules()
+        tutors = self.sc.get_tutors()
+        return render_template("schedules/create_schedule.html", **locals())
 
     @route('/center-manager/manage-tutor-schedules')
     def manage_tutor_schedules(self):
@@ -16,3 +25,23 @@ class SchedulesView(FlaskView):
 
     def view_tutor_schedules(self):
         return render_template('schedules/view_tutor_schedule.html', **locals())
+
+    @route('/create', methods=['post'])
+    def create_new_schedule(self):
+        now = (datetime.now())
+
+        start_time = str(json.loads(request.data).get('startTime'))
+        start_time = datetime.strptime(start_time, '%H:%M')
+        start_time = start_time.replace(year=int(now.strftime('%Y')), month=int(now.strftime('%m')), day=int(now.strftime('%d')))
+
+        end_time = str(json.loads(request.data).get('endTime'))
+        end_time = datetime.strptime(end_time, '%H:%M')
+        end_time = end_time.replace(year=int(now.strftime('%Y')), month=int(now.strftime('%m')), day=int(now.strftime('%d')))
+
+        is_active = str(json.loads(request.data).get('isActive'))
+        if is_active == 'Active':
+            is_active = 'Yes'
+        else:
+            is_active = 'No'
+        self.sc.create_schedule(start_time, end_time, is_active)
+        return redirect(url_for('SchedulesView:create_schedule'))
--- writing_center/schedules/schedules_controller.py
+++ writing_center/schedules/schedules_controller.py
@@ -1,3 +1,35 @@
+from sqlalchemy import orm
+from writing_center.db_repository import db_session
+from writing_center.db_repository.tables import UserTable, UserRoleTable, RoleTable, WCScheduleTable
+
+
 class SchedulesController:
     def __init__(self):
         pass
+
+    def get_schedules(self):
+        return db_session.query(WCScheduleTable)\
+            .all()
+
+    def create_schedule(self, start_time, end_time, is_active):
+        # TODO ADD CHECK FOR EXISTING SCHEDULES
+        new_schedule = WCScheduleTable(timeStart=start_time, timeEnd=end_time, isActive=is_active)
+        db_session.add(new_schedule)
+        db_session.commit()
+
+    def check_for_existing_schedule(self, start_time, end_time):
+        try:  # return true if there is an existing user
+            schedule = db_session.query(WCScheduleTable)\
+                .filter(WCScheduleTable.timeStart == start_time)\
+                .filter(WCScheduleTable.timeEnd == end_time)\
+                .one()
+            return True
+        except orm.exc.NoResultFound:  # otherwise return false
+            return False
+
+    def get_tutors(self):
+        return db_session.query(UserTable)\
+            .filter(UserTable.id == UserRoleTable.user_id)\
+            .filter(UserRoleTable.role_id == RoleTable.id)\
+            .filter(RoleTable.role == 'role_tutor')\
+            .all()
