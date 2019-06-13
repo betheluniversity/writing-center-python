9ebf41f974e6a4100f461146c39359f9baa83aee
Bailey Middendorf <bam95899@bethel.edu>
2019-06-13 14:36:04 -0500

    Added necessary functions

--- writing_center/appointments/__init__.py
+++ writing_center/appointments/__init__.py
@@ -1,16 +1,32 @@
 from flask_classy import FlaskView, route
 from flask import render_template
+from datetime import datetime
+
+from writing_center.appointments.appointments_controller import AppointmentsController
 
 
 class AppointmentsView(FlaskView):
-    route_base = ''
+    route_base = 'appointments'
 
     def __init__(self):
-        pass
+        self.ac = AppointmentsController()
 
-    @route('/center-manager/edit-appointments/view')
+    def index(self):
+        years = self.ac.get_years()
+        return render_template('appointments/select_view.html', **locals())
+
+    @route('/view-all')
     def view_all_appointments(self):
+        appointments = self.ac.get_filled_appointments()
+        get_name = self.ac.get_user_by_username
         return render_template('appointments/view_all_appointments.html', **locals())
+
+    @route('/view-yearly/<int:selected_year>')
+    def view_yearly_appointments(self, selected_year):
+        appointments = self.ac.get_yearly_appointments(selected_year)
+        get_name = self.ac.get_user_by_username
+        select_year = selected_year
+        return render_template('appointments/view_yearly_appointments.html', **locals())
 
     def appointments_and_walk_ins(self):
         return render_template('appointments/appointments_and_walk_ins.html', **locals())
--- writing_center/appointments/appointments_controller.py
+++ writing_center/appointments/appointments_controller.py
@@ -1,3 +1,42 @@
+from datetime import datetime
+
+from writing_center.db_repository import db_session
+from writing_center.db_repository.tables import UserTable, WCAppointmentDataTable
+
+
 class AppointmentsController:
     def __init__(self):
         pass
+
+    def get_user_by_username(self, username):
+        return db_session.query(UserTable)\
+            .filter(UserTable.username == username)\
+            .one_or_none()
+
+    def get_filled_appointments(self):
+        return db_session.query(WCAppointmentDataTable)\
+            .filter(WCAppointmentDataTable.StudUsername != "")\
+            .filter(WCAppointmentDataTable.StudUsername != None)\
+            .all()
+
+    def get_yearly_appointments(self, selected_year):
+        test = db_session.query(WCAppointmentDataTable)\
+            .filter(WCAppointmentDataTable.StudUsername != "")\
+            .filter(WCAppointmentDataTable.StudUsername != None)\
+            .all()
+        tt = []
+        for t in test:
+            if int(t.StartTime.strftime('%Y')) == selected_year:
+                tt.append(t)
+        return tt
+
+    def get_years(self):
+        years = [2014]
+        year = 2015
+        current_year = datetime.now()
+        current_year = int(current_year.strftime('%Y'))
+        while year <= current_year:
+            years.append(year)
+            year += 1
+        return years
+
