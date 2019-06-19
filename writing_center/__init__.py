4626c6f6dbde19516d1988b5904b4e14c838ab47
Boston KJ <bak45247@bethel.edu>
2019-06-19 09:09:51 -0500

    added vscode files to gitignore

--- writing_center/__init__.py
+++ writing_center/__init__.py
@@ -19,7 +19,7 @@ from writing_center.appointments import AppointmentsView
 from writing_center.cron import CronView
 from writing_center.profile import ProfileView
 from writing_center.schedules import SchedulesView
-# from writing_center.settings import SettingsView
+from writing_center.settings import SettingsView
 from writing_center.statistics import StatisticsView
 from writing_center.users import UsersView
 from writing_center.users.users_controller import UsersController as uc
@@ -31,7 +31,7 @@ AppointmentsView.register(app)
 CronView.register(app)
 ProfileView.register(app)
 SchedulesView.register(app)
-# SettingsView.register(app)
+SettingsView.register(app)
 StatisticsView.register(app)
 UsersView.register(app)
 
