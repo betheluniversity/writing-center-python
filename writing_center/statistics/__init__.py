f5acae2c5837ca73fe47799b36231fed4ca1f9ef
Bailey Middendorf <bam95899@bethel.edu>
2019-07-08 09:08:49 -0500

    Changed function to match existing WC

--- writing_center/statistics/__init__.py
+++ writing_center/statistics/__init__.py
@@ -36,15 +36,36 @@ class StatisticsView(FlaskView):
         end = datetime.strptime(end, '%a %b %d %Y')
         appointments = self.sc.get_appt_hours(start, end, flask_session['USERNAME'])
         time = 0
-        hours = 0
 
         for appointment in appointments:
-            # Gets the time in seconds
-            time += (appointment.actualEnd - appointment.actualStart).seconds
-        # Converts the seconds to hours
-        print(hours)
-        time /= 3600
-        time = round(time, 2)
+            start_time = str(appointment.actualStart).split(' ')[1].split(':')
+            start_min = int(start_time[1])
+            start_hour = int(start_time[0])
+            if 0 < start_min and start_min < 15:
+                start_min = 15
+            elif 15 < start_min and start_min < 30:
+                start_min = 30
+            elif 30 < start_min and start_min < 45:
+                start_min = 45
+            elif 45 < start_min and start_min < 60:
+                start_min = 0
+                if start_hour < 24:
+                    start_hour += 1
+            end_time = str(appointment.actualEnd).split(' ')[1].split(':')
+            end_min = int(end_time[1])
+            end_hour = int(end_time[0])
+            if 0 < end_min and end_min < 15:
+                end_min = 15
+            elif 15 < end_min and end_min < 30:
+                end_min = 30
+            elif 30 < end_min and end_min < 45:
+                end_min = 45
+            elif 45 < end_min and end_min < 60:
+                end_min = 0
+                if end_hour < 24:
+                    end_hour += 1
+            time += end_hour - start_hour + ((end_min - start_min) / 60)
+
         user = self.sc.get_user_by_username(flask_session['USERNAME'])
         start = start.strftime('%B %d %Y')
         end = end.strftime('%B %d %Y')
