5c8d77aa76586b045979475d8eebb50982684cd8
Jordan Jasik <joj28267@bethel.edu>
2019-07-12 09:33:24 -0500

    removed date_offset

--- writing_center/wsapi/wsapi_controller.py
+++ writing_center/wsapi/wsapi_controller.py
@@ -25,8 +25,8 @@ class WSAPIController:
         path = '/username/{0}/courses'.format(username)
         return self.get_hmac_request(path)
 
-    def get_course_info(self, course_dept, course_num, date_offset=0):
-        path = '/course/info/{0}/{1}/{2}'.format(course_dept, course_num, date_offset)
+    def get_course_info(self, course_dept, course_num):
+        path = '/course/info/{0}/{1}'.format(course_dept, course_num)
         return self.get_hmac_request(path)
 
     def validate_course(self, course_dept, course_num):
