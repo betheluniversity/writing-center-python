5093c50636cc12d1aa8581747fc3cd11f20718cf
Jordan Jasik <joj28267@bethel.edu>
2019-07-23 10:37:13 -0500

    Cleanse the system

--- writing_center/settings/settings_controller.py
+++ writing_center/settings/settings_controller.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 from writing_center.db_repository import db_session
-from writing_center.db_repository.tables import SettingsTable
+from writing_center.db_repository.tables import SettingsTable, UserTable, UserRoleTable, RoleTable
 
 
 class SettingsController:
@@ -12,4 +14,18 @@ class SettingsController:
     def update_setting(self, setting_name, new_setting):
         setting = db_session.query(SettingsTable).filter(SettingsTable.name == setting_name).one()
         setting.value = new_setting
+        db_session.commit()
+
+    def cleanse(self):
+        students = db_session.query(UserTable)\
+            .filter(UserTable.id == UserRoleTable.user_id)\
+            .filter(UserRoleTable.role_id == RoleTable.id)\
+            .filter(RoleTable.name == "Student").all()
+        for student in students:
+            roles = db_session.query(UserRoleTable).filter(UserRoleTable.user_id == student.id).all()
+            if len(roles) == 1:  # This means "Student" is their only role
+                student.deletedAt = datetime.now()
+        banned = db_session.query(UserTable).filter(UserTable.bannedDate != None).all()
+        for ban in banned:
+            ban.deletedAt = None
         db_session.commit()
