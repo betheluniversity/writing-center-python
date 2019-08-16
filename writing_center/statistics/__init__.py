1df74f7e2571364bd9364470ececebfe2df88e5d
Bailey Middendorf <bam95899@bethel.edu>
2019-08-16 09:42:11 -0500

    Changed get_appt_hours to actually return hours instead of just appts

--- writing_center/statistics/__init__.py
+++ writing_center/statistics/__init__.py
@@ -36,37 +36,7 @@ class StatisticsView(FlaskView):
         end = str(json.loads(request.data).get('end'))
         start = datetime.strptime(start, '%a %b %d %Y')
         end = datetime.strptime(end, '%a %b %d %Y')
-        appointments = self.sc.get_appt_hours(start, end, flask_session['USERNAME'])
-        time = 0
-
-        for appointment in appointments:
-            start_time = str(appointment.scheduledStart).split(' ')[1].split(':')
-            start_min = int(start_time[1])
-            start_hour = int(start_time[0])
-            if 0 < start_min < 15:
-                start_min = 15
-            elif 15 < start_min < 30:
-                start_min = 30
-            elif 30 < start_min < 45:
-                start_min = 45
-            elif 45 < start_min < 60:
-                start_min = 0
-                if start_hour < 24:
-                    start_hour += 1
-            end_time = str(appointment.scheduledEnd).split(' ')[1].split(':')
-            end_min = int(end_time[1])
-            end_hour = int(end_time[0])
-            if 0 < end_min < 15:
-                end_min = 15
-            elif 15 < end_min < 30:
-                end_min = 30
-            elif 30 < end_min < 45:
-                end_min = 45
-            elif 45 < end_min < 60:
-                end_min = 0
-                if end_hour < 24:
-                    end_hour += 1
-            time += end_hour - start_hour + (end_min - start_min) / 60
+        time = self.sc.get_appt_hours(start, end, flask_session['USERNAME'])
 
         user = self.sc.get_user_by_username(flask_session['USERNAME'])
         start = start.strftime('%B %d %Y')
