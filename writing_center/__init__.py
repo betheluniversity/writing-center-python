5cfef59dd3ec8287a69409d7119b199b902a01d0
Jacob Jensen <jacob-jensen@bethel.edu>
2023-11-10 09:21:17 -0600

    allow to run behind reverse proxy

--- writing_center/__init__.py
+++ writing_center/__init__.py
@@ -22,6 +22,7 @@ from writing_center.message_center import MessageCenterView
 from writing_center.appointments import AppointmentsView
 from writing_center.cron import CronView
 from writing_center.profile import ProfileView
+from writing_center.proxy import ReverseProxied
 from writing_center.schedules import SchedulesView
 from writing_center.settings import SettingsView
 from writing_center.statistics import StatisticsView
@@ -40,6 +41,10 @@ SettingsView.register(app)
 StatisticsView.register(app)
 UsersView.register(app)
 GoogleCalendarView.register(app)
+
+# Allow app to work behind a reverse proxy
+if app.config.get('PROXY_PATH'):
+    app.wsgi_app = ReverseProxied(app.wsgi_app, script_name=app.config.get('PROXY_PATH'))
 
 
 # This makes these variables open to use everywhere
